5e0171e773faa4043b908d36f60a3141c47f1240
Samuel Gale <sam.gale@byu.net>
2017-04-06 11:56:10 -0600

    Fine tuning some bugs

--- blur/content.js
+++ blur/content.js
@@ -1,53 +1,96 @@
-function blur() {
-	const images = document.querySelectorAll('img');
-	for (const image of images) {
-    	image.style.filter = "brightness(250%) blur(7px) contrast(50%) opacity(1)";
-    	image.addEventListener('mouseover', reveal);
-    	image.addEventListener('mouseout', unreveal);
-  	}
-}
-
-function unblur() {
-	const images = document.querySelectorAll('img');
-	for (const image of images) {
-    	image.style.filter = "opacity(1)";
-  	}
-}
-
-function onLoad(e) {
-	chrome.storage.sync.get('blurOnDefault', function(values){
-		if (values.blurOnDefault) {
-			blur();
-		} else {
-			unblur();
-		}
-	});
-}
-
-function reveal(e) {
-    window.blurTimer = setTimeout(() => {
-        this.style.filter = "opacity(1)";
-    }, 1200);
-}
-
-function unreveal(e) {
-	clearTimeout(window.blurTimer);
-	this.style.filter = "brightness(250%) blur(7px) contrast(50%) opacity(1)";
-}
-
-document.addEventListener('DOMContentLoaded', onLoad);
-document.addEventListener('DOMSubtreeModified', onLoad);
-
-chrome.runtime.onMessage.addListener(
-	function(request, sender, sendResponse) {
-		console.log("Blurring...");
-		
-		if (request.status === "blur") {
-			blur();
-		} else if (request.status === "unblur") {
-			unblur();
-		}
-
-		sendResponse("Complete");
+(function(){
+	function blur(image) {
+		image.style.filter = "blur(6px) opacity(1)";
 	}
-);
+
+	function show(image) {
+		image.style.filter = "opacity(1)";
+	}
+
+	function blurAll() {
+		const images = document.querySelectorAll('img');
+		for (const image of images) {
+			blur(image);
+	  	}
+	  	window.imageBlurState = "blurred";
+	}
+
+	function revealAll() {
+		const images = document.querySelectorAll('img');
+		for (const image of images) {
+	    	show(image);
+	  	}
+	  	window.imageBlurState = "revealed";
+	}
+
+	function reveal(e) {
+		if (window.imageBlurState === "blurred") {
+		    window.blurTimer = setTimeout(() => {
+		        show(e.target);
+		    }, 500);
+		}
+	}
+
+	function unreveal(e) {
+		if (window.imageBlurState === "blurred") {
+			clearTimeout(window.blurTimer);
+			blur(e.target);
+		}
+	}
+
+	function initialBlurAll() {
+		const images = document.querySelectorAll('img');
+		for (const image of images) {
+			if (image.dataset.imageBlurOnLoadUpdateOccured != "true") {
+				console.log(image.dataset.imageBlurOnLoadUpdateOccured != true, image.dataset.imageBlurOnLoadUpdateOccured);
+				blur(image);
+				image.addEventListener('mouseover', reveal);
+				image.addEventListener('mouseout', unreveal);
+				image.dataset.imageBlurOnLoadUpdateOccured = true;
+			}
+	  	}
+	  	window.imageBlurState = "blurred";
+	}
+
+	function initialLoadRevealAll() {
+		const images = document.querySelectorAll('img');
+		for (const image of images) {
+			if (image.dataset.imageBlurOnLoadUpdateOccured != "true") {
+	    		show(image);
+	    		image.addEventListener('mouseover', reveal);
+				image.addEventListener('mouseout', unreveal);
+	    		image.dataset.imageBlurOnLoadUpdateOccured = true;
+	    	}
+	  	}
+	  	window.imageBlurState = "revealed";
+	}
+
+	function onPageLoad(e) {
+		chrome.storage.sync.get('blurOnDefault', function(values){
+			if (values.blurOnDefault) {
+				initialBlurAll();
+			} else {
+				initialLoadRevealAll();
+			}
+		});
+	}
+
+	document.addEventListener('DOMContentLoaded', function(e) {
+		onPageLoad(e);
+		document.addEventListener('DOMNodeInserted', onPageLoad);
+	});
+
+	chrome.runtime.onMessage.addListener(
+		function(request, sender, sendResponse) {
+			console.log("Blurring...");
+			
+			if (request.status === "blur") {
+				blurAll();
+			} else if (request.status === "unblur") {
+				revealAll();
+			}
+
+			sendResponse("Complete");
+		}
+	);
+})()
